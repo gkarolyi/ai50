51bb9475150b395349f7d866d76f0c4ee83ceded
gkarolyi <33122882+gkarolyi@users.noreply.github.com>
2024-12-17 16:56:49 +1100

    week 6 parser almost done

---
+++ 6_language/parser/parser.py
@@ -0,0 +1,101 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+# holmes sat in the armchair in the home
+# N V P Det N P Det N
+# NP (N) VP (V) PP (P) NP (Det N) PP (P) NP (Det N)
+
+# Holmes sat down and lit his pipe
+# N V Adv Conj V Det N
+# NP (N) VP (V) Adv Conj VP (V) NP (Det N)
+
+# She never said a word
+# N Adv V Det N
+# NP (N) Adv VP (V) Det N Conj NP (N) VP (V) P Det N Adv
+
+# holmes sat in the armchair in the home his companion arrived at.
+# N V P Det N P Det N Det N V P
+# NP (N) VP (V) PP (P) NP (Det N) PP (P) NP (Det N) NP (Det N) VP (V) PP (P)
+
+
+NONTERMINALS = """
+S -> NP VP | S Conj S
+NP -> N | Det NP | Adj NP | NP PP
+PP -> P NP | P S
+VP -> V | V NP | V PP | V NP PP | VP Conj VP | VP Adv | Adv VP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        # s = input("Sentence: ")
+        s = "holmes sat in the armchair in the home"
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    return [token.lower() for token in nltk.tokenize.word_tokenize(sentence) if token.isalpha()]
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    def filter(t): return t.label() == "NP" and not any(child.label() == "NP" for child in t)
+    return [chunk for chunk in tree.subtrees(filter)]
+
+
+if __name__ == "__main__":
+    main()
