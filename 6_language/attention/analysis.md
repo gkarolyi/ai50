4e3d2409827016f29b6dd3618824ab6c9e20ec24
gkarolyi <33122882+gkarolyi@users.noreply.github.com>
2024-12-21 16:43:32 +1100

    add attention implementation and analysis

--- 6_language/attention/analysis.md
+++ 6_language/attention/analysis.md
@@ -1,18 +1,31 @@
 # Analysis
 
-## Layer TODO, Head TODO
+## Layer 5, head 6
 
-TODO
+For this head, determiners are strongly related to the nouns to which they belong.
 
-Example Sentences:
-- TODO
-- TODO
+Sentences:
+- "The hungry cat caught a [MASK] mouse in the kitchen."
+- "The [MASK] teacher wrote long notes on the board."
+- "A [MASK] bird flew over the tall tree yesterday."
 
-## Layer TODO, Head TODO
+## Layer 6, head 4
 
-TODO
+This head seems to link verbs with their associated prepositions. It shows a correlation
+between word pairs like 'chased-across', 'gave-to' and 'carried-through'.
 
-Example Sentences:
-- TODO
-- TODO
+Sentences:
+- "The small dog chased the [MASK] cat across the yard."
+- "The old man gave the [MASK] book to his friend."
+- "A tall woman carried the [MASK] bag through the store."
 
+
+## Layer 3 Head 7
+
+This head pays attention to itself irrespective of position within the sentence.
+
+Example sentences:
+- "His big dog chased the [MASK] cat."
+- "A small [MASK] ran across the yard."
+- "My [MASK] bird flew over the tree."
+- "The tall man [MASK] in the corner."
--- 6_language/attention/mask.py
+++ 6_language/attention/mask.py
@@ -45,9 +45,13 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
-    # TODO: Implement this function
-    raise NotImplementedError
+    input_ids = inputs.input_ids[0].numpy()
 
+    for index, token_id in enumerate(input_ids):
+        if token_id == mask_token_id:
+            return index
+
+    return None
 
 
 def get_color_for_attention_score(attention_score):
@@ -55,9 +59,8 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    # TODO: Implement this function
-    raise NotImplementedError
-
+    grey = round(attention_score.numpy() * 255)
+    return (grey, grey, grey)
 
 
 def visualize_attentions(tokens, attentions):
@@ -70,13 +73,9 @@ def visualize_attentions(tokens, attentions):
     include both the layer number (starting count from 1) and head number
     (starting count from 1).
     """
-    # TODO: Update this function to produce diagrams for all layers and heads.
-    generate_diagram(
-        1,
-        1,
-        tokens,
-        attentions[0][0][0]
-    )
+    for layer_number, layer in enumerate(attentions, start=1):
+        for head_number, attention_weights in enumerate(layer[0], start=1):
+            generate_diagram(layer_number, head_number, tokens, attention_weights)
 
 
 def generate_diagram(layer_number, head_number, tokens, attention_weights):
